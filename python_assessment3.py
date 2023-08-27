38803174eccc4d2eeeaed1bfdec3d6e0f26a71d7
SYDMAZHAR <142782345+SYDMAZHAR@users.noreply.github.com>
2023-08-27 14:57:45 +0530

    Add files via upload

---
+++ python_assessment3.py
@@ -0,0 +1,12 @@
+def sum_of_numbers(num_list):
+    total = sum(num_list)
+    return total
+
+# Sample List
+sample_list = [8, 2, 3, 0, 7]
+
+# Calculating the sum
+result = sum_of_numbers(sample_list)
+
+# Printing the result
+print("Expected Output:", result)
